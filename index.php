0ec2848a9bbbbc48be98e0e59026c79a4db62157
Marchel Adias Pradana <marchel.adias@gmail.com>
2023-01-19 08:23:31 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, cumque ipsum reprehenderit distinctio quam cum nobis delectus magnam. Nam, placeat at? Molestiae quas est quidem eum. Exercitationem dolore saepe unde.</p>
+    <ol>
+        <li></li>
+        <li></li>
+    </ol>
 </body>
 </html>
